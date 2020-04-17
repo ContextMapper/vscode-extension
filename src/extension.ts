031325930ac44192e2affc5a156e3d991e562039
Stefan Kapferer <stefan@kapferer.ch>
2020-04-17 15:36:55 +0200

    First step towards VS Code extension for Context Mapper

---
+++ src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+
+import * as path from 'path';
+import * as os from 'os';
+
+import {Trace} from 'vscode-jsonrpc';
+import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/mydsl
+    let launcher = os.platform() === 'win32' ? 'context-mapper-lsp.bat' : 'context-mapper-lsp';
+    let script = context.asAbsolutePath(path.join('lsp', 'bin', launcher));
+
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        debug: { command: script, args: [], options: { env: createDebugEnv() } }
+    };
+    
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['cml'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    let lc = new LanguageClient('CML Language Server', serverOptions, clientOptions);
+    
+    var disposable2 =commands.registerCommand("cml.a.proxy", async () => {
+        let activeEditor = window.activeTextEditor;
+        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'cml') {
+            return;
+        }
+
+        if (activeEditor.document.uri instanceof Uri) {
+            commands.executeCommand("cml.a", activeEditor.document.uri.toString());
+        }
+    })
+    context.subscriptions.push(disposable2);
+    
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.trace = Trace.Verbose;
+    let disposable = lc.start();
+    
+    // Push the disposable to the context's subscriptions so that the 
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+}
+
+function createDebugEnv() {
+    return Object.assign({
+        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
+    }, process.env)
+}
